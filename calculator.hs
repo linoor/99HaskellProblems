e3a0f2292c6bc02c25b6ae744d831e8081b99588
= <michal.pomaranski@gmail.com>
2015-11-20 17:33:30 +0100

    added maybe to calculator

--- calculator.hs
+++ calculator.hs
@@ -1,13 +1,22 @@
 import Data.List
+import Control.Monad
 
-solveRPN :: String -> Float
-solveRPN expression = head $ foldl foldingFunction [] (words expression)
+solveRPN :: String -> Maybe Double
+solveRPN expression = do
+			[result] <- foldM foldingFunction [] (words expression)
+			return result
 	where
-		foldingFunction (x:y:ys) "*"    = (x*y):ys
-		foldingFunction (x:y:ys) "+"    = (x+y):ys
-		foldingFunction (x:y:ys) "-"    = (y-x):ys
-		foldingFunction (x:y:ys) "/"    = (y/x):ys
-		foldingFunction (x:y:ys) "^"    = (y**x):ys
-		foldingFunction (x:xs) "ln"     = log x:xs
-		foldingFunction xs "sum"        = [sum xs]
-		foldingFunction xs numberString = read numberString:xs
+		foldingFunction :: [Double] -> String -> Maybe [Double]
+		foldingFunction (x:y:ys) "*"    = return $ (x*y):ys
+		foldingFunction (x:y:ys) "+"    = return $ (x+y):ys
+		foldingFunction (x:y:ys) "-"    = return $ (y-x):ys
+		foldingFunction (x:y:ys) "/"    = return $ (y/x):ys
+		foldingFunction (x:y:ys) "^"    = return $ (y**x):ys
+		foldingFunction (x:xs) "ln"     = return $ log x:xs
+		foldingFunction xs "sum"        = return $ [sum xs]
+		foldingFunction xs numberString = liftM (:xs) (readMaybe numberString)
+
+readMaybe :: (Read a) => String -> Maybe a
+readMaybe st = case reads st of
+				[(x, "")] -> Just x
+				_         -> Nothing
